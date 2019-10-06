f52ffcb473fa829230d1994e2532c7a4d2ddd944
Jonathan Tangpos <jonathan.tangpos@gmail.com>
2019-10-06 10:31:09 +0800

    install bootstrap and created home, task-details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
